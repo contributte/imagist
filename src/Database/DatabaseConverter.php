8fba335be178758be9b1bedbfa11fb501b940fa7
MartkCz <martk494@gmail.com>
2022-08-12 14:04:07 +0200

    cs fix

--- src/Database/DatabaseConverter.php
+++ src/Database/DatabaseConverter.php
@@ -70,7 +70,7 @@ final class DatabaseConverter implements DatabaseConverterInterface
 			return $nullable ? null : new EmptyImage();
 		}
 
-		return new $this->className($value); // @phpcs:ignore
+		return new $this->className($value);
 	}
 
 }
