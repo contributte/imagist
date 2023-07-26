ef5383202b84339e9686c4c9c251418f92383a16
MartkCz <martk494@gmail.com>
2023-07-26 16:36:29 +0200

    add ImagistNormalizer

---
+++ src/Bridge/Symfony/Serializer/ImagistNormalizer.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types = 1);
+
+namespace Contributte\Imagist\Bridge\Symfony\Serializer;
+
+use Contributte\Imagist\Entity\EmptyImageInterface;
+use Contributte\Imagist\Entity\PersistentImageInterface;
+use Contributte\Imagist\Filter\FilterInterface;
+use Contributte\Imagist\LinkGeneratorInterface;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+final class ImagistNormalizer implements NormalizerInterface
+{
+
+	public const AS_OBJECT = 'imagist.asObject';
+	public const FILTERS = 'imagist.filters';
+	public const FILTERS_BY_SCOPE = 'imagist.filtersByScope';
+
+	private LinkGeneratorInterface $linkGenerator;
+
+	public function __construct(LinkGeneratorInterface $linkGenerator)
+	{
+		$this->linkGenerator = $linkGenerator;
+	}
+
+	/**
+	 * @param mixed $object
+	 * @param mixed[] $context
+	 * @return PersistentImageInterface|array<string|null>|string|null
+	 */
+	public function normalize($object, string $format = null, array $context = [])
+	{
+		assert($object instanceof PersistentImageInterface);
+
+		if (($context[self::AS_OBJECT] ?? false) === true) {
+			return $object;
+		}
+
+		$filters = $context[self::FILTERS] ?? [];
+
+		if ($filters) {
+			return $this->applyFilters($object, $filters);
+		}
+
+		$filters = ($context[self::FILTERS_BY_SCOPE] ?? [])[$object->getScope()->toString()] ?? [];
+
+		if ($filters) {
+			return $this->applyFilters($object, $filters);
+		}
+
+		if ($object instanceof EmptyImageInterface) {
+			return null;
+		}
+
+		return $this->linkGenerator->link($object);
+	}
+
+	/**
+	 * @param array<string, FilterInterface> $filters
+	 * @return array<string|null>
+	 */
+	private function applyFilters(PersistentImageInterface $image, array $filters): array
+	{
+		$links = [];
+
+		foreach ($filters as $name => $filter) {
+			$links[$name] = $this->linkGenerator->link($image->withFilter($filter));
+		}
+
+		return $links;
+	}
+
+	/**
+	 * @param mixed $data
+	 */
+	public function supportsNormalization($data, string $format = null): bool
+	{
+		return $data instanceof PersistentImageInterface;
+	}
+
+}
